8ba179de561159529e867c659186c120dfea37ea
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-04-27 23:14:06 +0300

    AttributeUsage eklendi.

--- GenericsExample/Attributes/RequiredPropertyAttribute.cs
+++ GenericsExample/Attributes/RequiredPropertyAttribute.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace GenericsExample.Attributes
 {
+    [AttributeUsage(AttributeTargets.All)]
+    //oluşturduğunda attribute için de tanımlı olan attributeleri kullanabilirsin
+    //örnek olarak attributeusage senin attribute yapının nerelerde kullanılabileceğini tutuyor.
     class RequiredPropertyAttribute:Attribute
     {
 
